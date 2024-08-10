f246a349f155ef8901399058806bc37b4726e2e7
Ben Anderson <dataknut@icloud.com>
2024-08-10 19:00:50 +1200

    initial set up - install packages needed

---
+++ setup.R
@@ -0,0 +1,10 @@
+# install libraries for testing
+
+# https://cran.r-project.org/web/packages/future/vignettes/future-1-overview.html
+install.packages("futureverse")
+
+# futures and furrr
+
+# snow
+# https://cran.r-project.org/web/packages/snow/index.html
+install.packages("snow")
